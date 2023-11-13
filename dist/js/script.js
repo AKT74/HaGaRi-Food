bae0c81ac2502408807634279f8fc06811961d2e
Gading Khairlambang <sasakihaise985@gmail.com>
2023-11-13 12:35:43 +0700

    hagari

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,29 @@
-window.onscroll = function(){
-    const header = document.querySelector('header')
+document.getElementById('starters').addEventListener('click', function () {
+    document.getElementById('starterSection').classList.remove('hidden');
+    document.getElementById('lunchSection').classList.add('hidden');
+});
+
+// Fungsi untuk menangani klik pada tombol Lunch
+document.getElementById('lunch').addEventListener('click', function () {
+    document.getElementById('lunchSection').classList.remove('hidden');
+    document.getElementById('starterSection').classList.add('hidden');
+});
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
-    if(window.pageYOffset > fixedNav){
+    if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
-    } else{
+    } else {
         header.classList.remove('navbar-fixed');
     }
-}
+});
 
-const hamburger = document.querySelector('#hamburger')
-const navMenu = document.querySelector('#nav-menu')
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function(){
+hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
-})
+});
